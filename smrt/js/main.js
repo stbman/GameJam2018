61db0d9e857abebcf84ed59a9355c658d90ed7fc
quarbby <lynnette-ng@outlook.com>
2018-01-27 13:32:34 +0800

    Add event click

--- smrt/js/main.js
+++ smrt/js/main.js
@@ -1,24 +1,17 @@
-var trainList = [{
-  "name": "PENGUIN STOP",
-  "coords": "488,98,6",
-  "id": "TEST",
-  "alt": "help"
-  }
-]
-
-//var hide = false;
-
 $(document).ready(function() {
 	
 	for (var i=0; i<trainList.length; i++) {
 		var train = trainList[i];
 		
 		var trainElem = "<area class=\"mrt-map-area\" id=\"" + train["id"] + "\" shape=\"circle\" coords=\"" + train["coords"] + "\"" + " href=\"#\"  alt=\"" + train["alt"] + "\">";  
-		console.log(trainElem);
 		$("#mrtmap").append(trainElem);
 	}
 	
-    $("area.mrt-map-area").mouseover(function (event) {
+	$("#map-container").click(function (event) {
+	    console.log(event.clientX + " " + event.clientY);
+	});
+
+	$("area.mrt-map-area").mouseover(function (event) {
 		/*
         if (hide) {
             clearTimeout(hide);
---
+++ smrt/js/trains.js
@@ -0,0 +1,7 @@
+var trainList = [{
+    "name": "PENGUIN STOP",
+    "coords": "488,98,6",
+    "id": "TEST",
+    "alt": "help"
+}
+]

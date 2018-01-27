b063e945df8f193b74e719ce23f13062fd5aa90f
quarbby <lynnette-ng@outlook.com>
2018-01-27 15:03:37 +0800

    add lrt lines

---
+++ smrt/js/seline.js
@@ -0,0 +1,12 @@
+var seline = [
+
+    {"id":"NE16/STC"     , "coords":"622,139,6" , "alt":"Sengkang"},
+    {"id":"SE1"          , "coords":"664,161,4" , "alt":"Compassvale"},
+    {"id":"SE2"          , "coords":"678,175,4" , "alt":"Rumbia"},
+    {"id":"SE3"          , "coords":"677,195,4" , "alt":"Bakau"},
+    {"id":"SE4"          , "coords":"655,197,4" , "alt":"Kangkar"},
+    {"id":"SE5"          , "coords":"643,183,4" , "alt":"Ranggung"},
+
+]
+
+allTrainLists.push(seline);
---
+++ smrt/js/swline.js
@@ -0,0 +1,14 @@
+var swline = [
+
+    {"id":"NE16/STC"     , "coords":"622,139,6" , "alt":"Sengkang"},
+    {"id":"SW1"          , "coords":"604,111,4" , "alt":"Cheng Lim"},
+    {"id":"SW2"          , "coords":"598,93,4"  , "alt":"Farmway"},
+    {"id":"SW3"          , "coords":"587,81,4"  , "alt":"Kupang"},
+    {"id":"SW4"          , "coords":"570,75,4"  , "alt":"Thanggam"},
+    {"id":"SW5"          , "coords":"559,86,4"  , "alt":"Fernvale"},
+    {"id":"SW6"          , "coords":"563,101,4" , "alt":"Layar"},
+    {"id":"SW7"          , "coords":"573,112,4" , "alt":"Tongkang"},
+    {"id":"SW8"          , "coords":"593,121,4" , "alt":"Renjong"}
+]
+
+allTrainLists.push(swline);
